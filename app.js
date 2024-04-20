d82fd0c470a15f5db5cf259d271a4b2a0c93ee27
Valeria <valeria.torrealba@gmail.com>
2024-04-20 12:05:02 -0400

    modificaciones de la ruta de la imagen

--- app.js
+++ app.js
@@ -25,23 +25,11 @@ app.get("/imagen", async(req, res) =>{
     const { urlImagen } = req.query
     const uuid = uuidv4();
     const codigo = uuid.slice(0, 6);
-    const imagen = await Jimp.read(urlImagen);
-    
+    const imagen = await Jimp.read(urlImagen);    
     await imagen
         .resize(350, Jimp.AUTO)
         .greyscale()
-        .writeAsync(`${codigo}.png`)
-    const imagenData = fs.readFileSync(`${codigo}.png`);   
+        .writeAsync(`${codigo}.jpg`)
+    const imagenData = fs.readFileSync(`${codigo}.jpg`);   
     res.send(imagenData)
-
 });
-
-// app.get("/", async (req, res) =>{
-//     const imagen = await Jimp.read("https://images.pexels.com/photos/20922645/pexels-photo-20922645/free-photo-of-nieve-nevar-paisaje-arena.jpeg")
-//     await imagen
-//     .resize(250, Jimp.AUTO)
-//     .sepia()
-//     .writeAsync('img.png')    
-//     const imagenData = fs.readFileSync('img.png')    
-//     res.send(imagenData)
-// })
